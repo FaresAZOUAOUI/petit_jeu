e904b3453c75a8b272273f65cbc8b0e45b2de0ce
Fares_AZOUAOUI <fares.azouaoui@gmail.com>
2024-09-25 13:58:35 +0200

    Methode1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 ''' TP de Farès AZOUAOUI'''
+
+''' Imports eventuels'''
+
+import random as rd
+
+''' Programme principal'''
+
+class Jeu :
+    def __init__(self, n, m):
+        self.k = rd.randint(0,m)
+        self.n = n
