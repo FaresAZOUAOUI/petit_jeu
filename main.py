32fc6d0192e34a34dcb44d7241539d20cd8ec900
Fares_AZOUAOUI <fares.azouaoui@gmail.com>
2024-09-25 14:41:58 +0200

    Methode3

--- main.py
+++ main.py
@@ -8,10 +8,24 @@ import random as rd
 
 class Jeu :
     def __init__(self, n, m):
+        '''Constructeur de la classe Jeu
+        
+        Paramètres: m, la borne maximale du tirage et n, le nombre maximal d'essais
+        '''
+
         self.k = rd.randint(0,m)
         self.n = n
     
     def test(self, k) :
+        '''Compare le nombre deviné avec celui a deviner
+        
+        Paramètres : k, le nombre a deviner
+        
+        Returns : Booléen: 
+        True si le nombre est le même, 
+        False si il est trop petit ou trop grand 
+        '''
+
         if k != self.k :
             if k < self.k :
                 print("Trop petit")
@@ -21,6 +35,24 @@ class Jeu :
         else :
             print("Super tu as gagné t'es un monstre")
             return True
+        
+    def jouer(self) :
+        ''' Cette méthode fait une boucle jusqu'à ce que le nombre d'essais maximum,n ,soit atteint
+        ou jusqu'à ce que le nombre soit trouvé
+
+        Returns : Booléen
+        '''
+
+        n = 0
+        while n != self.n :
+            try :
+                k = int(input("Entre un nombre"))
+                self.test(k)
+                n += 1
+                print("Il vous reste", self.n,"essais")
+            except ValueError :
+                print("Ceci n'est pas un entier")
+
 
 ''' Programme principal'''
 
